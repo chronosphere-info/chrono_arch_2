4831ccfe9d1ad28b50abd8f57d76d5a0d6d6c944
adamkocsis <adam.t.kocsis@gmail.com>
2023-05-04 13:58:32 +0200

    Update to version 20230504_b12

---
+++ code/R/10_meow.R
@@ -0,0 +1,15 @@
+#' @param na logical flag to trigger the removal of placeholder values - it also forces the dataset in the memory!
+
+assignInNamespace(
+	"loadVar", 
+	function(dir, verbose=FALSE, attach=TRUE){
+        if(! requireNamespace("sf", quietly=TRUE)) stop("This dataset requires the 'sf' package to load.")
+	
+		if(attach){
+			library("sf")
+		}
+
+		sf <- sf::st_read(file.path(dir, "MEOW/meow_ecos.shx"), quiet=!verbose)
+		return(sf)
+	}, 
+	ns="chronosphere")
