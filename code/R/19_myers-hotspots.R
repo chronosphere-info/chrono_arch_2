37e2c6f82fa809f7baa021835b1756b3581f6143
adamkocsis <adam.t.kocsis@gmail.com>
2023-05-05 14:37:00 +0200

    Update to version 20230505_b23

---
+++ code/R/19_myers-hotspots.R
@@ -0,0 +1,20 @@
+#' @param validate logical flag indicating whether sf::st_make_valid should be run on the output (default to TRUE)
+
+assignInNamespace(
+	"loadVar", 
+	function(dir, verbose=FALSE, attach=TRUE, validate=TRUE){
+        if(! requireNamespace("sf", quietly=TRUE)) stop("This dataset requires the 'sf' package to load.")
+	
+		if(attach){
+			library("sf")
+		}
+
+		sf <- sf::st_read(file.path(dir, "hotspots_2016_1.shx"), quiet=!verbose)
+
+		if(validate & verbose) {
+			message("Running sf::st_make_valid(), use 'validate=FALSE' to skip.")
+			sf <- sf::st_make_valid()
+		} 
+		return(sf)
+	}, 
+	ns="chronosphere")
