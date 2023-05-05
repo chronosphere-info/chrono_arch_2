88ea78ee6a9fdc3ab8d2515cb75648a5dde97c69
adamkocsis <adam.t.kocsis@gmail.com>
2023-05-05 10:40:44 +0200

    Update to version 20230505_b18

---
+++ code/R/16_triton_pres.R
@@ -0,0 +1,18 @@
+#' Function to load in a specific variable
+
+#' @param dir path to temporary directory.
+#' @param verbose  Should feedback be output to the console?
+#' @param attach  Should feedback be output to the console?
+
+assignInNamespace(
+	"loadVar", 
+	function(dir, verbose=FALSE, attach=FALSE){
+		# read in the data
+		thisEnv <- environment()
+		# load and input
+		load(file.path(dir, "triton.RData"), envir=thisEnv)
+
+		# return
+		return(triton.pres)
+	}, 
+	ns="chronosphere")
