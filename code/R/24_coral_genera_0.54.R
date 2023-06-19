8213f13725b7cb59e8c538c96591bf551ed3aa2a
adamkocsis <adam.t.kocsis@gmail.com>
2023-06-19 11:03:57 +0200

    Update to version 20230619_b33

---
+++ code/R/24_coral_genera_0.54.R
@@ -0,0 +1,17 @@
+#' Function to load in a specific variable
+
+#' @param dir path to temporary directory.
+#' @param verbose  Should feedback be output to the console?
+#' @param attach  Should feedback be output to the console?
+assignInNamespace(
+	"loadVar", 
+	function(dir, verbose=FALSE, attach=FALSE){
+		# read the data 
+		dat <- read.csv(file.path(dir, "coralList0.54.csv"), sep=";")
+
+		return(dat)
+	}, 
+	ns="chronosphere")
+
+
+
