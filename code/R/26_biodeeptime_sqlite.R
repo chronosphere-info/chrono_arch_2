327c42e2d28aeaf6cd35c9b53f2ab8e4153f465d
adamkocsis <adam.t.kocsis@gmail.com>
2023-07-18 12:30:40 +0200

    Update to version 20230718_b58

---
+++ code/R/26_biodeeptime_sqlite.R
@@ -0,0 +1,26 @@
+#' Function to load in a specific variable
+
+#' @param dir path to temporary directory.
+#' @param verbose  Should feedback be output to the console?
+#' @param attach  Should feedback be output to the console?
+
+assignInNamespace(
+	"loadVar", 
+	function(dir, verbose=FALSE, attach=FALSE){
+
+        if(! requireNamespace("RSQLite", quietly=TRUE)) stop("This dataset requires the 'RSQLite' package to load.")
+        if(! requireNamespace("DBI", quietly=TRUE)) stop("This dataset requires the 'DBI' package to load.")
+	
+	
+		if(attach){
+			library("RSQLite")
+			library("DBI")
+		}
+
+		# read in the data
+		con <- DBI::dbConnect(RSQLite::SQLite(), file.path(dir, "bdt.sqlite"))
+
+		# return
+		return(con)
+	}, 
+	ns="chronosphere")
