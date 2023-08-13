82e5cc4854ad340f196abf93b94a04a2e172a55e
Ádám T. Kocsis <adam.kocsis@fau.de>
2023-08-13 14:29:12 +0200

    Update to version 20230813_b78

---
+++ code/R/29_biodeeptime-bchron-neotoma.R
@@ -0,0 +1,16 @@
+#' Function to load in a specific variable
+
+#' @param dir path to temporary directory.
+#' @param verbose  Should feedback be output to the console?
+#' @param attach  Should feedback be output to the console?
+
+assignInNamespace(
+	"loadVar", 
+	function(dir, verbose=FALSE, attach=FALSE){
+		# read in the data
+		dat <- readRDS(file.path(dir, "neotoma_bchron.rds"))
+
+		# return
+		return(dat)
+	}, 
+	ns="chronosphere")
