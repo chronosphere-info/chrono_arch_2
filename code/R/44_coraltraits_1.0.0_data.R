9e4e37973f6b4de7a438d378afb95af8a4bee334
adamkocsis <adam.t.kocsis@gmail.com>
2023-11-10 16:58:39 +0100

    Update to version 20231110_b164

---
+++ code/R/44_coraltraits_1.0.0_data.R
@@ -0,0 +1,16 @@
+#' Function to load in a specific variable
+
+#' @param dir path to temporary directory.
+#' @param verbose  Should feedback be output to the console?
+#' @param attach  Should feedback be output to the console?
+
+assignInNamespace(
+	"loadVar", 
+	function(dir, verbose=FALSE, attach=FALSE){
+		# read in the data
+		dat <- read.csv(file.path(dir,"ctdb_1.0.0","ctdb_1.0.0_data.csv"))
+
+		# return
+		return(dat)
+	}, 
+	ns="chronosphere")
