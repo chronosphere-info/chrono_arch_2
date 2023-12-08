a61fffa61b5edb7e23edcf220b44c6ac33366fdd
adamkocsis <adam.t.kocsis@gmail.com>
2023-12-08 15:02:10 +0100

    Update to version 20231208_b210

---
+++ code/R/63_forcis5_pump.R
@@ -0,0 +1,19 @@
+#' Function to load in a specific variable
+
+#' @param dir path to temporary directory.
+#' @param verbose  Should feedback be output to the console?
+#' @param attach  Should feedback be output to the console?
+
+assignInNamespace(
+	"loadVar", 
+	function(dir, verbose=FALSE, attach=FALSE){
+		ver <- "v05"
+		date <- "14092023"
+		thing <- "pump"
+		# read in the data
+		dat <- read.csv(file.path(dir,paste0("FORCIS_", thing, "_", ver, "_", date, ".csv" )), sep=";")
+
+		# return
+		return(dat)
+	}, 
+	ns="chronosphere")
