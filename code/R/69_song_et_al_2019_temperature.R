203c6d5a909bd2c905f1c5901f81013ee46c5b31
adamkocsis <adam.t.kocsis@gmail.com>
2023-12-20 13:50:56 +0100

    Update to version 20231220_b227

---
+++ code/R/69_song_et_al_2019_temperature.R
@@ -0,0 +1,23 @@
+#' Function to load in a specific variable
+
+#' @param dir path to temporary directory.
+#' @param verbose  Should feedback be output to the console?
+#' @param attach  Should feedback be output to the console?
+
+assignInNamespace(
+	"loadVar", 
+	function(dir, verbose=FALSE, attach=FALSE){
+
+        if(! requireNamespace("openxlsx", quietly=TRUE)) stop("This dataset requires the 'openxlsx' package to load.")
+	
+		if(attach){
+			library("openxlsx")
+		}
+
+		# read in the data
+		dat <- openxlsx::read.xlsx(file.path(dir, "12583_2018_1002_MOESM1_ESM.xlsx"), sheet=3, startRow=2)
+
+		# return
+		return(dat)
+	}, 
+	ns="chronosphere")
