16cc5610bdd0e9dc96192ca6a263b8f4790e37ee
adamkocsis <adam.t.kocsis@gmail.com>
2024-05-31 17:59:34 +0200

    Update to version 20240531_b342

---
+++ code/R/77_veizer2015temperatures_surface.R
@@ -0,0 +1,62 @@
+#' Function to load in a specific variable
+
+#' @param dir path to temporary directory.
+#' @param verbose  Should feedback be output to the console?
+#' @param attach  Should feedback be output to the console?
+
+assignInNamespace(
+	"loadVar", 
+	function(dir, verbose=FALSE, attach=FALSE){
+
+        if(! requireNamespace("readxl", quietly=TRUE)) stop("This dataset requires the 'readxl' package to load.")
+	
+		if(attach){
+			library("readxl")
+			if(verbose) message("Attaching the 'readxl' package")
+		}
+
+		# the tabs
+		ntabs <- 26 
+
+		# read in the data
+		allData <- list()
+
+		# column names aggregator
+		columns <- NULL
+		
+		# read in all the data
+		for(i in 1:ntabs){
+			suppressWarnings(allData[[i]] <- readxl::read_excel(file.path(dir, "1-s2.0-S0012825215000604-mmc2.xls"), sheet=i+1))
+			allData[[i]] <- as.data.frame(allData[[i]])
+
+			# column name aggregation
+			columns <- unique(c(columns, colnames(allData[[i]])))
+		}
+		columns <- sort(columns)
+
+		# the merged data
+		merged <- data.frame()
+		for(i in 1:length(allData)){
+			# simplify
+			current<- allData[[i]]
+			present <- colnames(current)
+			# missing
+			missing <- columns[!columns%in%present]
+			
+			# if match is not possible
+			# add empty columns
+			if(length(missing)>0){
+				for(j in 1:length(missing)){
+					current[[missing[j]]] <- NA
+				}
+			}
+
+			# force to same order
+			merged <-rbind(merged, current[, columns])
+
+		}
+
+		# return
+		return(merged)
+	}, 
+	ns="chronosphere")
