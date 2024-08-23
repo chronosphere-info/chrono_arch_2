fcfb52d8ee4721091f2b852ca39a6377b1fe75f2
adamkocsis <adam.t.kocsis@gmail.com>
2024-08-23 14:21:11 +0200

    one more correction to 80

--- code/R/80_paleomap-dem2.R
+++ code/R/80_paleomap-dem2.R
@@ -3,9 +3,8 @@
 #' @param dir path to archive directory
 #' @param verbose  Should feedback be output to the console?
 #' @param attach  Should the packages on which the dataset depends be attached?
-#' @param flipdiff Some files do not follow standards and are read in differently, should these be flipped?
 assignInNamespace("loadVar",
-	function(dir, verbose=FALSE, attach=TRUE, flipdiff=FALSE){
+	function(dir, verbose=FALSE, attach=TRUE){
 		if(! requireNamespace("ncdf4", quietly=TRUE)) stop("This dataset requires the 'ncdf4' package to load.")
 		if(! requireNamespace("terra", quietly=TRUE)) stop("This dataset requires the 'terra' package to load.")
 		if(! requireNamespace("via", quietly=TRUE)) stop("This dataset requires the 'via' package to load.")
@@ -149,8 +148,9 @@ assignInNamespace("loadVar",
 		names(pa) <- all
 
 
-		if(verbose) message("Note that with some GDAL versions the 75Ma and 100Ma rasters are flipped.",
-			"\nThis can be prevented with with 'fetch(..., flipdiff=TRUE)'.")
+		# some versions of GDAL make this issue
+		needFlip <- terra::values(pa[[16]])[1] > -1000
+
 		if(flipdiff){
 			pa[[16]] <- terra::flip(pa[[16]])
 			pa[[21]] <- terra::flip(pa[[21]])
