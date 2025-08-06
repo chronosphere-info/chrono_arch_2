35e996913b7929b852fc1ad1f3ac23ff40d999d0
adamkocsis <adam.t.kocsis@gmail.com>
2025-08-06 17:35:18 +0200

    Update to version 20250806_b571

---
+++ code/R/88_dsmsni-0.1.R
@@ -0,0 +1,20 @@
+#' Function to load in a specific variable
+
+#' @param dir path to temporary directory.
+#' @param verbose  Should feedback be output to the console?
+
+assignInNamespace(
+	"loadVar", 
+	function(dir, verbose=FALSE){
+
+		# uses chinese traditional encoding
+		dat <- read.csv(file.path(dir,"DSMSNI_v0.1.csv"), fileEncoding="GB18030")
+
+		# clean  up the file (empty column + apparent leftovers)
+		dat$X <- NULL
+		dat$X.1 <- NULL
+
+		# return
+		return(dat)
+	}, 
+ns="chronosphere")
